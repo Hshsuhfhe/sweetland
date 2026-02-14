ec467f2892b62fce98c34f721f1068a2b1cdf00e
Hshsuhfhe <genpro1993@gmail.com>
2026-02-14 17:42:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,9 @@ navigator.clipboard.writeText("play.sweet-land.fun");
 
 const notice=document.getElementById("copyNotice");
 notice.style.opacity="1";
-notice.style.transform="translate(-50%,-50%) scale(1)";
 
 setTimeout(()=>{
 notice.style.opacity="0";
-notice.style.transform="translate(-50%,-50%) scale(.8)";
 },2000);
 }
 
@@ -16,11 +14,7 @@ document.body.classList.toggle("light");
 document.body.classList.toggle("dark");
 }
 
-function togglePanel(){
-document.getElementById("sidePanel").classList.toggle("open");
-}
-
-/* АНИМАЦИИ ВНИЗ-ВВЕРХ */
+/* повтор анимаций */
 const observer=new IntersectionObserver(entries=>{
 entries.forEach(entry=>{
 if(entry.isIntersecting){
@@ -29,13 +23,13 @@ entry.target.classList.add("show");
 entry.target.classList.remove("show");
 }
 });
-},{threshold:0.25});
+},{threshold:0.2});
 
 document.querySelectorAll(".animate").forEach(el=>{
 observer.observe(el);
 });
 
-/* СНЕГ */
+/* снег */
 const canvas=document.getElementById("snow");
 const ctx=canvas.getContext("2d");
 
@@ -43,7 +37,8 @@ canvas.width=window.innerWidth;
 canvas.height=window.innerHeight;
 
 let snowflakes=[];
-for(let i=0;i<100;i++){
+
+for(let i=0;i<120;i++){
 snowflakes.push({
 x:Math.random()*canvas.width,
 y:Math.random()*canvas.height,
@@ -56,10 +51,12 @@ function drawSnow(){
 ctx.clearRect(0,0,canvas.width,canvas.height);
 ctx.fillStyle="white";
 ctx.beginPath();
+
 snowflakes.forEach(f=>{
 ctx.moveTo(f.x,f.y);
 ctx.arc(f.x,f.y,f.r,0,Math.PI*2,true);
 });
+
 ctx.fill();
 moveSnow();
 }
@@ -75,3 +72,8 @@ f.x=Math.random()*canvas.width;
 }
 
 setInterval(drawSnow,33);
+
+window.addEventListener("resize",()=>{
+canvas.width=window.innerWidth;
+canvas.height=window.innerHeight;
+});

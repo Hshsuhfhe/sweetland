cd2454f4ff18da0384e253ef1cff529832df43fc
Hshsuhfhe <genpro1993@gmail.com>
2026-02-14 16:26:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,16 +2,21 @@
 function copyIP(){
 navigator.clipboard.writeText("play.sweet-land.fun");
 
-const notice = document.getElementById("copyNotice");
-notice.style.opacity = "1";
+const notice=document.getElementById("copyNotice");
+notice.style.opacity="1";
 
 setTimeout(()=>{
 notice.style.opacity="0";
 },2000);
 }
 
-// scroll анимации повторяемые
-const observer = new IntersectionObserver(entries=>{
+// темы
+function setTheme(theme){
+document.body.className=theme;
+}
+
+// повторяющиеся анимации
+const observer=new IntersectionObserver(entries=>{
 entries.forEach(entry=>{
 if(entry.isIntersecting){
 entry.target.classList.add("show");
@@ -19,9 +24,7 @@ entry.target.classList.add("show");
 entry.target.classList.remove("show");
 }
 });
-},{
-threshold:0.2
-});
+},{threshold:0.2});
 
 document.querySelectorAll(".animate").forEach(el=>{
 observer.observe(el);

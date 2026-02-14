f9931dc26cdb6d496703d529cba863c3e2833be2
Hshsuhfhe <genpro1993@gmail.com>
2026-02-14 14:52:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+// Появление элементов при скролле
+const animatedElements = document.querySelectorAll('.animate');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if(entry.isIntersecting){
+      entry.target.style.animationPlayState = 'running';
+      observer.unobserve(entry.target);
+    }
+  });
+}, {threshold: 0.1});
+
+animatedElements.forEach(el => {
+  observer.observe(el);
+});
 

19d25f5da9b8d54a0bc5627974c400541ef020eb
Hshsuhfhe <genpro1993@gmail.com>
2026-02-14 15:44:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-// Анимация появления элементов с плавным выездом
-document.addEventListener("DOMContentLoaded", () => {
-  const animatedElements = document.querySelectorAll(".animate");
-
-  animatedElements.forEach((el, index) => {
-    setTimeout(() => {
-      el.classList.add("visible");
-    }, index * 200); // задержка между элементами
-  });
+// появление элементов
+const observer=new IntersectionObserver(entries=>{
+entries.forEach(e=>{
+if(e.isIntersecting){
+e.target.classList.add("show");
+}
 });
+});
+
+document.querySelectorAll(".animate").forEach(el=>observer.observe(el));
+
+// копирование IP
+function copyIP(){
+navigator.clipboard.writeText("play.sweet-land.fun");
+
+const n=document.getElementById("copyNotice");
+n.style.opacity="1";
+n.style.transform="translateY(0)";
+
+setTimeout(()=>{
+n.style.opacity="0";
+n.style.transform="translateY(20px)";
+},2000);
+}
